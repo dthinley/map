84e3250e89bfcb65d5236acb00191b51c8c12ee6
Dorjee Thinlay <dthinley@yahoo.com>
2018-01-17 15:25:35 -0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,29 @@
+    var initialLocations = [
+	{
+		name: 'Golden Gate',
+		lat: 37.819929,
+		long: -122.478255
+	},
+	{
+		name: 'AT&T Park',
+		lat: 37.7749 ,
+		long: -122.4194
+	},
+	{
+		name: 'Fisherman wharf',
+		lat: 37.808,
+		long: -122.4177
+	},
+	{
+		name: 'Alcatraz Island',
+		lat: 37.82662,
+		long: -122.4228
+	},
+
+	{
+		name: 'California Academy of Sciences',
+		lat: 37.76989,
+		long: -122.4663
+	},
+    ];
+    
