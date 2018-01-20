0811d3ef54ea61ae94523958d1f054046cd4c238
Dorjee Thinlay <dthinley@yahoo.com>
2018-01-20 12:17:11 -0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -26,4 +26,53 @@
 		long: -122.4663
 	},
     ];
-    
+        var initialLocations = [{
+            name: 'Golden Gate',
+            lat: 37.819929,
+            long: -122.478255
+        }, {
+            name: 'AT&T Park',
+            lat: 37.7749,
+            long: -122.4194
+        }, {
+            name: 'Fisherman wharf',
+            lat: 37.808,
+            long: -122.4177
+        }, {
+            name: 'Alcatraz Island',
+            lat: 37.82662,
+            long: -122.4228
+        },
+
+        {
+            name: 'California Academy of Sciences',
+            lat: 37.76989,
+            long: -122.4663
+        },
+
+        {
+            name: 'Palace of Fine Arts Theatre',
+            lat: 37.801478,
+            long: -122.448175
+        },
+
+        {
+            name: 'de Young Museum',
+            lat: 37.771389,
+            long: -122.468611
+        }, {
+            name: 'Japanese Tea Garden',
+            lat: 37.770091,
+            long: -122.470436
+        },
+
+        {
+            name: 'Ferry Building Marketplace',
+            lat: 37.7955,
+            long: -122.3933
+        }, {
+            name: 'Twin Peaks',
+            lat: 37.7544,
+            long: -122.4477
+        },
+    ];
