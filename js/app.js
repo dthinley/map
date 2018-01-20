48d3a0d5c33c3e51b529635a08b9084e805afce5
Dorjee Thinlay <dthinley@yahoo.com>
2018-01-20 12:14:01 -0800

    style: removed white space

--- js/app.js
+++ js/app.js
@@ -2,123 +2,128 @@ var map;
 var clientID;
 var clientSecret;
 var Location = function(data) {
-	var self = this;
+    var self = this;
 
-	this.name = data.name;
-	this.lat = data.lat;
-	this.long = data.long;
-	this.URL = "";
-	this.street = "";
-	this.city = "";
-	this.phone = "";
+    this.name = data.name;
+    this.lat = data.lat;
+    this.long = data.long;
+    this.URL = "";
+    this.street = "";
+    this.city = "";
+    this.phone = "";
 
-	this.visible = ko.observable(true);
+    this.visible = ko.observable(true);
 
-	var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.name;
+    var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll=' + this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.name;
 
-	$.getJSON(foursquareURL).done(function(data) {
-		var results = data.response.venues[0];
-		self.URL = results.url;
-		self.street = results.location.formattedAddress[0];
-     	self.city = results.location.formattedAddress[1];
-      	self.phone = results.contact.phone;
-      	
-	}).fail(function() {
-		alert("Error found. Please refresh your page.");
-	});
+    $.getJSON(foursquareURL).done(function(data) {
+        var results = data.response.venues[0];
+        self.URL = results.url;
+        self.street = results.location.formattedAddress[0];
+        self.city = results.location.formattedAddress[1];
+        self.phone = results.contact.phone;
 
-	this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+    }).fail(function() {
+        alert("Error found. Please refresh your page.");
+    });
+
+    this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL + '">' + self.URL + "</a></div>" +
         '<div class="content">' + self.street + "</div>" +
         '<div class="content">' + self.city + "</div>" +
         '<div class="content">' + self.phone + "</div></div>";
 
-	this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
+    this.infoWindow = new google.maps.InfoWindow({
+        content: self.contentString
+    });
 
-	this.marker = new google.maps.Marker({
-			position: new google.maps.LatLng(data.lat, data.long),
-			map: map,
-			title: data.name
-	});
+    this.marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.lat, data.long),
+        map: map,
+        title: data.name
+    });
 
-	this.showMarker = ko.computed(function() {
-		if(this.visible() === true) {
-			this.marker.setMap(map);
-		} else {
-			this.marker.setMap(null);
-		}
-		return true;
-	}, this);
+    this.showMarker = ko.computed(function() {
+        if (this.visible() === true) {
+            this.marker.setMap(map);
+        } else {
+            this.marker.setMap(null);
+        }
+        return true;
+    }, this);
 
-	this.marker.addListener('click', function(){
-		self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
-        '<div class="content">' + self.street + "</div>" +
-        '<div class="content">' + self.city + "</div>" +
-        '<div class="content"><a href="tel:' + self.phone +'">' + self.phone +"</a></div></div>";
+    this.marker.addListener('click', function() {
+        self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+            '<div class="content"><a href="' + self.URL + '">' + self.URL + "</a></div>" +
+            '<div class="content">' + self.street + "</div>" +
+            '<div class="content">' + self.city + "</div>" +
+            '<div class="content"><a href="tel:' + self.phone + '">' + self.phone + "</a></div></div>";
 
         self.infoWindow.setContent(self.contentString);
 
-		self.infoWindow.open(map, this);
+        self.infoWindow.open(map, this);
 
-		self.marker.setAnimation(google.maps.Animation.BOUNCE);
-      	setTimeout(function() {
-      		self.marker.setAnimation(null);
-     	}, 2100);
-	});
+        self.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            self.marker.setAnimation(null);
+        }, 2100);
+    });
 
-	this.bounce = function(place) {
-		google.maps.event.trigger(self.marker, 'click');
-	};
+    this.bounce = function(place) {
+        google.maps.event.trigger(self.marker, 'click');
+    };
 };
 
 //-ViewModel
 function AppViewModel() {
-	var self = this;
+    var self = this;
 
-	this.searchLocation = ko.observable("");
-	
-	this.locationList = ko.observableArray([]);
+    // create array of places
 
-	map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 12,
-			center: {lat: 37.77986, lng: -122.429}
-	});
+    this.locationList = ko.observableArray([]);
+    this.searchLocation = ko.observable('');
 
-	// Foursquare API settings
-	clientID = "VZ2JT2MO24F4I1FISB1WHSM0UTMWCLMBF0GUVBBTOB5WXOAD";
-	clientSecret = "PQSBTUDULE4ML240LQKMH0EL5Y2SJQSDXNMKIQKJMGVCNMDH";
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: {
+            lat: 37.77986,
+            lng: -122.429
+        }
+    });
 
-	initialLocations.forEach(function(locationItem){
-		self.locationList.push( new Location(locationItem));
-	});
+    // Foursquare API settings
+    clientID = "VZ2JT2MO24F4I1FISB1WHSM0UTMWCLMBF0GUVBBTOB5WXOAD";
+    clientSecret = "PQSBTUDULE4ML240LQKMH0EL5Y2SJQSDXNMKIQKJMGVCNMDH";
 
-	this.filteredList = ko.computed( function() {
-		var filter = self.searchLocation().toLowerCase();
-		if (!filter) {
-			self.locationList().forEach(function(locationItem){
-				locationItem.visible(true);
-			});
-			return self.locationList();
-		} else {
-			return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
-				var string = locationItem.name.toLowerCase();
-				var result = (string.search(filter) >= 0);
-				locationItem.visible(result);
-				return result;
-			});
-		}
-	}, self);
+    initialLocations.forEach(function(locationItem) {
+        self.locationList.push(new Location(locationItem));
+    });
 
-	this.mapElem = document.getElementById('map');
-	this.mapElem.style.height = window.innerHeight - 50;
+    this.filteredList = ko.computed(function() {
+        var filter = self.searchLocation().toLowerCase();
+        if (!filter) {
+            self.locationList().forEach(function(locationItem) {
+                locationItem.visible(true);
+            });
+            return self.locationList();
+        } else {
+            return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
+                var string = locationItem.name.toLowerCase();
+                var result = (string.search(filter) >= 0);
+                locationItem.visible(result);
+                return result;
+            });
+        }
+    }, self);
+
+    this.mapElem = document.getElementById('map');
+    this.mapElem.style.height = window.innerHeight - 50;
 }
-
+// Bind the VeiewModel to the view using knockout
 function startApp() {
-	ko.applyBindings(new AppViewModel());
+    ko.applyBindings(new AppViewModel());
 }
 
 function errorHandling() {
-	alert("Error loading page, try refresh in page.");
+    alert("Error loading page, try refresh in page.");
 }
-
